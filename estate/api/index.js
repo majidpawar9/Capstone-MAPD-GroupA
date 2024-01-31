a2734ea323d431f21a269368a7bbc667b68bb39a
majidpawar9 <majidpawar9@gmail.com>
2024-01-30 21:21:48 -0500

    connected database

--- estate/api/index.js
+++ estate/api/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect("mongodb+srv://majidpawar:majidpawar@estate.dg4lgyq.mongodb.net/?retryWrites=true&w=majority")
 
 const app = express();
 
 app.listen(3000,() => {
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port 3000!!!");
 });
