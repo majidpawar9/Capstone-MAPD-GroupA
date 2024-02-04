d5b936522f5e72848cba4be79c5ee9537319a043
majidpawar9 <majidpawar9@gmail.com>
2024-02-03 20:29:10 -0500

    created and tested api route

--- estate/api/index.js
+++ estate/api/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
 
 mongoose.connect("mongodb+srv://majidpawar:majidpawar@estate.dg4lgyq.mongodb.net/?retryWrites=true&w=majority")
 
@@ -8,3 +9,5 @@ const app = express();
 app.listen(3000,() => {
     console.log("Server is running on port 3000!!!");
 });
+
+app.use('/api/user', userRouter);
