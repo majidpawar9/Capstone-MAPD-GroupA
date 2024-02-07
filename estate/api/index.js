ab483b2d9fdc9ac7d2869a97ea38b38836aae5e9
majidpawar9 <majidpawar9@gmail.com>
2024-02-06 19:50:02 -0500

    created signup API route

--- estate/api/index.js
+++ estate/api/index.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 mongoose.connect("mongodb+srv://majidpawar:majidpawar@estate.dg4lgyq.mongodb.net/?retryWrites=true&w=majority")
 
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000,() => {
     console.log("Server is running on port 3000!!!");
 });
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
