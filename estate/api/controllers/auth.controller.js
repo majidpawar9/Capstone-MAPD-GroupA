5b90ef45716f1ef01d845d16c303ff5a90e54cf7
majidpawar9 <majidpawar9@gmail.com>
2024-02-11 01:28:42 -0500

    added signup functionality

--- estate/api/controllers/auth.controller.js
+++ estate/api/controllers/auth.controller.js
@@ -11,6 +11,6 @@ export const signup = async (req,res, next) => {
         res.status(201).json("User created successfully!");
     }
     catch(error){
-        next(errorHandler(550, 'error from the function'));
+        next(errorHandler(550, error.body));
     }
 };
