f4fb2dacd7a8174a8816c4c18781101dc0fc3528
majidpawar9 <majidpawar9@gmail.com>
2024-02-13 21:56:02 -0500

    created sign in api route

--- estate/api/controllers/auth.controller.js
+++ estate/api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
+import process from 'process';
 
 export const signup = async (req,res, next) => {
     const {username,email,password} = req.body;
@@ -11,6 +13,22 @@ export const signup = async (req,res, next) => {
         res.status(201).json("User created successfully!");
     }
     catch(error){
-        next(errorHandler(550, error.body));
+        next(error);
     }
 };
+
+export const signin = async (req, res, next) =>{
+    const {email, password } = req.body;
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, 'User not found!'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401, 'Wrong Credentials!'));
+        const token = jwt.sign({id: validUser.id},"adgaudadtu12712127");
+        const {password: pass, ...rest} = validUser._doc;
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
+    }
+    catch(error){
+        next(error);
+    }
+}
